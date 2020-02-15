b8ddf5124b7e614f1f5a1f417c6c5ebc4e3e487c
Ｎick <zhan16061@gmail.com>
2020-02-15 21:51:19 +0800

    Creating our lottery numbers

--- lottery.py
+++ lottery.py
@@ -1,7 +1,19 @@
+import random
+
+
 def getPlayerNumbers():
     userNumbers = input("輸入你要投注的號碼 6 隔，並請使用逗點分開。如 '1,2,3,4,5,6'\n：")
     userNumbersList = userNumbers.split(',')
     userNumbersSet = {int(number) for number in userNumbersList}
     return userNumbersSet
 
-print(getPlayerNumbers())
+
+def createLotteryNumbers():
+    numberSet = set()  # 初始化 set，請勿使用 {}
+    while len(numberSet) < 6:
+        numberSet.add(random.randint(1, 46))
+    return numberSet
+
+
+print('你輸入的號碼為：{playerNumbers}'.format(playerNumbers=getPlayerNumbers()))
+print('樂透號碼為：{lotteryNumbers}'.format(lotteryNumbers=createLotteryNumbers()))
