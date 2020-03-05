6eb75819dd1acaec10f13b8ae89ca275a34bd93e
Ｎick <zhan16061@gmail.com>
2020-03-05 21:08:55 +0800

    calculating the lottery winnings

--- lottery.py
+++ lottery.py
@@ -1,5 +1,10 @@
 import random
 
+def menu():
+    userNumbers = getPlayerNumbers()
+    lotteryNumbers = createLotteryNumbers()
+    matchNumbers = userNumbers.intersection(lotteryNumbers)
+    print('中獎號碼為{}, 中獎金額為{}'.format(matchNumbers, 1000 * len(matchNumbers)))
 
 def getPlayerNumbers():
     userNumbers = input("輸入你要投注的號碼 6 隔，並請使用逗點分開。如 '1,2,3,4,5,6'\n：")
@@ -11,9 +16,10 @@ def getPlayerNumbers():
 def createLotteryNumbers():
     numberSet = set()  # 初始化 set，請勿使用 {}
     while len(numberSet) < 6:
-        numberSet.add(random.randint(1, 46))
+        numberSet.add(random.randint(1, 10))
     return numberSet
 
 
-print('你輸入的號碼為：{playerNumbers}'.format(playerNumbers=getPlayerNumbers()))
-print('樂透號碼為：{lotteryNumbers}'.format(lotteryNumbers=createLotteryNumbers()))
+# print('你輸入的號碼為：{playerNumbers}'.format(playerNumbers=getPlayerNumbers()))
+# print('樂透號碼為：{lotteryNumbers}'.format(lotteryNumbers=createLotteryNumbers()))
+menu()
